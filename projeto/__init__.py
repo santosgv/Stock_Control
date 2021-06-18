076043a7a372bb4f5f6794b5903d20408eeed602
vitor.patrus <54445515+d4rk3n3ss@users.noreply.github.com>
2021-06-18 18:49:35 -0300

    Create __init__.py
    teste

---
+++ projeto/__init__.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+main=Tk()
+
+class aplicacao():
+    def __init__(self):
+        self.main=main
+        self.Principal()
+        main.mainloop()
+
+    def Principal(self):
+        self.main.title('Planilha')
+        self.main.geometry('1024x1024')
+        self.main.resizable(False, False)
+aplicacao()
