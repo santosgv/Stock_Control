a4ad9223084d0ce643797698d756f8ba048abdf2
vitor.patrus <54445515+d4rk3n3ss@users.noreply.github.com>
2021-06-22 22:30:48 -0300

    Update models.py

--- projeto/models.py
+++ projeto/models.py
@@ -3,28 +3,25 @@ import sqlite3
 
 class BancoDados():
     def __init__(self):
-        self.conn=sqlite3.connect('planilhadb')
-        self.cursor=self.conn.cursor()
-        self.cursor.execute("""
-             CREATE TABLE IF NOT EXISTS Acoes (
-                 COD INTEGER PRIMARY KEY,
-                 ACAO CHAR(40) NOT NULL,
-                 ValorCompra INTEGER(10),
-                 Quantidade int,
-                 ValorAtual money,
-                 Volume money,
-                 Ganho int,
-                 PQ char(200),              
-             );
-         """)
-        self.conn.commit()
+        self.conn = sqlite3.connect("planilha")
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS Acoes(
+        cod integer primary key,
+        ACAO char(10) not null,
+        ValorCompra money,
+        Quantidade int,
+        ValorAtual money,
+        Volume int,
+        Ganho char(10),
+        PQ char(200)
+        );          
+         ''')
 
-    def insert(self,acao,valorcompra,quantidade,valoratual,volume,ganho,porque):
+    def insert(self, acao, valorcompra, quantidade, valoratual, volume, ganho, porque):
         self.conn.execute(f'''
-            insert into Acoes (ACAO,ValorCompra,Quantidade,ValorAtual,Volume,Ganho,PQ) values('{acao}',{valorcompra},{quantidade},{valoratual},{volume},{ganho},'{porque}')
-        ''')
-        self.conn.commit()
-        self.conn.close()
+            insert into Acoes values('{acao}',{valorcompra},{quantidade},{valoratual},{volume},{ganho},{porque})
+        '''),self.conn.commit()
         return print('Inserido com sucesso')
 
     def update(self):
@@ -32,3 +29,5 @@ class BancoDados():
 
     def delete(self):
         pass
+a=BancoDados()
+a.insert('123',10,11,15,100,'100%','comprei')
