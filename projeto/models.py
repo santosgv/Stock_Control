bc2c16312cdeeabcf6fd0c049a2f3ada9ee40a6d
vitor.patrus <54445515+d4rk3n3ss@users.noreply.github.com>
2021-06-23 20:57:01 -0300

    Update models.py

--- projeto/models.py
+++ projeto/models.py
@@ -1,33 +1,40 @@
 import sqlite3
+banco = sqlite3.connect('planilha.db')
+cursor = banco.cursor()
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS PLANILHA(
+               ID INTEGER PRIMARY KEY,
+               ACAO CHAR(6)NOT NULL,
+               VLCOMPRA MONEY NOT NULL,
+               QT INT NOT NULL,
+               VLATUAL MONEY NOT NULL,
+               VOLUME INT NOT NULL,
+               GANHO FLOAT,
+               PQ VARCHAR(200)
+               );
+               ''')
+class Banco():
+    def __init__(self,ACAO,VLCOMPRA,QT,VLATUAL,VOLUME,GANHO,PQ):
+        self.banco = sqlite3.connect('planilha.db')
+        self.cursor = self.banco.cursor()
+        self.acao=ACAO
+        self.vlcompra=VLCOMPRA
+        self.qt=QT
+        self.vlatual=VLATUAL
+        self.vol=VOLUME
+        self.gn=GANHO
+        self.pq=PQ
 
-
-class BancoDados():
-    def __init__(self):
-        self.conn = sqlite3.connect("planilha")
-        self.cursor = self.conn.cursor()
+    def insert(self):
         self.cursor.execute(f'''
-        CREATE TABLE IF NOT EXISTS Acoes(
-        cod integer primary key,
-        ACAO char(10) not null,
-        ValorCompra money,
-        Quantidade int,
-        ValorAtual money,
-        Volume int,
-        Ganho char(10),
-        PQ char(200)
-        );          
-         ''')
+        INSERT INTO PLANILHA (ACAO,VLCOMPRA,QT,VLATUAL,VOLUME,GANHO,PQ) VALUES('{str(self.acao)}',{self.vlcompra},{self.qt},{self.vlatual},{self.vol},{self.gn},'{str(self.pq)}')
+        '''), self.banco.commit()
+        return print('inserido')
 
-    def insert(self, acao, valorcompra, quantidade, valoratual, volume, ganho, porque):
-        self.conn.execute(f'''
-            insert into Acoes values('{acao}',{valorcompra},{quantidade},{valoratual},{volume},{ganho},{porque})
-        '''),self.conn.commit()
-        return print('Inserido com sucesso')
+    def show(self):
+        dados=self.cursor.execute(f'''
+        select * from PLANILHA
+        ''')
+        for i in dados.fetchall():
+            return i
 
-    def update(self):
-        pass
-
-    def delete(self):
-        pass
-a=BancoDados()
-a.insert('123',10,11,15,100,'100%','comprei')
