3c1174dba2ee66c92c882ae839c1dc8b08b00ebe
vitor.patrus <54445515+d4rk3n3ss@users.noreply.github.com>
2021-06-22 22:29:57 -0300

    Update fucoes.py

--- projeto/fucoes.py
+++ projeto/fucoes.py
@@ -3,10 +3,11 @@ from .principal import *
 
 
 class FuncionalidadeS(BancoDados):
-    def adiciona(self,Aplicacao):
+    def __init__(self):
+        Aplicacao()
+    def adiciona(self):
         banco=BancoDados()
         banco.insert(self.entacao.get(),self.entvr.get(),self.entq.get(),'100,0',2,100,self.entpc.get())
-        print('adicionado com sucesso')
 
     def altera(self):
         self.update()
