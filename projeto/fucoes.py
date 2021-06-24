5285efcea1ffeb7d96bf2e770f88817d79ccde65
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2021-06-24 11:06:23 -0300

    Update fucoes.py

--- projeto/fucoes.py
+++ projeto/fucoes.py
@@ -1,4 +1,4 @@
-from .models import  Banco
+from .models import Banco, cursor
 from .principal import *
 
 
@@ -6,6 +6,7 @@ class FuncionalidadeS(Banco):
     def __init__(self):
         pass
     def adiciona(self):
+
         Banco(self.entacao.get(),self.entvr.get(),self.entq.get(),100,100,100,self.entpc.get()).insert()
 
     def altera(self):
